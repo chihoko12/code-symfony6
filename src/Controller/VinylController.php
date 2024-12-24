173e6b453fd8935b5523e56113c351e19d5c304c
Chihoko Neudeck <61164812+chihoko12@users.noreply.github.com>
2024-12-24 14:28:29 +0100

    feat: create a controller for routes

---
+++ src/Controller/VinylController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class VinylController
+{
+
+  #[Route('/')]
+  public function homepage(): Response
+  {
+    // die('Vinyl: Definitely NOT a fancy-loking frisbee!');
+    return new Response('Title: PB and Jams');
+  }
+
+  #[Route('/browse/{slug}')]
+  public function browse(string $slug = null): Response
+  {
+    if ($slug) {
+      $title = 'Genre: ' .ucfirst(str_replace('-', ' ', $slug));
+    } else {
+      $title = 'All Genres';
+    }
+
+    return new Response($title);
+  }
+
+}
