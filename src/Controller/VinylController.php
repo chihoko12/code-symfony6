03f1a15514c62cf18963a18dad545328664679ec
Chihoko Neudeck <61164812+chihoko12@users.noreply.github.com>
2024-12-30 16:39:27 +0100

    feat: create a homepage template

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -4,14 +4,32 @@ namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class VinylController
+class VinylController extends AbstractController
 {
 
   #[Route('/')]
   public function homepage(): Response
   {
-    return new Response('Title: PB and Jams');
+
+    $tracks = [
+      ['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
+      ['song' => 'Waterfalls', 'artist' => 'TLC'],
+      ['song' => 'Creep', 'artist' => 'Radiohead'],
+      ['song' => 'Kiss From A Rose', 'artist' => 'Seal'],
+      ['song' => 'On Bended Knee', 'artist' => 'Boyz II Men'],
+      ['song' => 'Fantasy', 'artist' => 'Mariah Carey'],
+      ['song' => 'Take A Bow', 'artist' => 'Madonna'],
+      ['song' => 'You Are Not Alone', 'artist' => 'Michael Jackson'],
+      ['song' => 'You Oughta Know', 'artist' => 'Alanis Morissette'],
+      ['song' => 'One Sweet Day', 'artist' => 'Mariah Carey & Boyz II'],
+    ];
+
+    return $this->render('vinyl/homepage.html.twig', [
+      'title' => 'PB & Jams',
+      'tracks' => $tracks,
+    ]);
   }
 
   #[Route('/browse/{slug}')]
