e15d2e3c13c8f342b5f216b161c3f0aaac74b513
Chihoko Neudeck <61164812+chihoko12@users.noreply.github.com>
2024-12-31 20:49:19 +0100

    feat: add logger

--- src/Controller/SongController.php
+++ src/Controller/SongController.php
@@ -6,19 +6,23 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
-
+use Psr\Log\LoggerInterface;
 
 class SongController extends AbstractController
 {
 
   #[Route('/api/songs/{id<\d+>}', name: 'app_song_getsong', methods: ['GET'])]
-  public function getSong(int $id): Response {
+  public function getSong(int $id, LoggerInterface $logger): Response {
 
     $song = [
       'id' => $id,
       'name' => 'Waterfalls',
       'url' => 'https://symfonycasts.s3.amazonaws.com/sample.mp3',
     ];
+
+    $logger->info('Returning API response for song {song}', [
+      'song' => $id,
+    ]);
 
     return $this->json($song);
 
--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use function Symfony\Component\String\u;
 
 class VinylController extends AbstractController
 {
@@ -35,7 +36,7 @@ class VinylController extends AbstractController
   #[Route('/browse/{slug}', name: 'app_browse')]
   public function browse(string $slug = null): Response
   {
-    $genre = $slug ? ucfirst(str_replace('-', ' ', $slug)) : null;
+    $genre = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
 
     return $this->render('vinyl/browse.html.twig', [
       'genre' => $genre,
